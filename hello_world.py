491dec3564e4536ff84e4747feeb8019ee63719c
slicepaperwords <chris@shout.net>
2018-12-04 22:20:08 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+def hello_world():
+    print("BooYa x 3!!")
+
+def hello():
+    print("hello yo")
+
+def totes():
+    print("this is totes.")
+
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
+hello_world()
+hello()
+totes()
